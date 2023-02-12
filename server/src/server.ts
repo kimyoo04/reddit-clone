0cb63b678e3e0d76d1d3112ccdb6f47b3944e27b
Kim Yoo <kimyoo04eco@naver.com>
2023-02-12 14:09:15 +0900

    Create server signup process

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import { AppDataSource } from "./data-source";
+import authRoutes from "./routes/auth";
 import cors from "cors";
 
 const app = express();
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 app.get("/", (_, res) => res.send("running"));
+app.use("/api/auth", authRoutes);
 
 let port = 4000;
 app.listen(port, async () => {

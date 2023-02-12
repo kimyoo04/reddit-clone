871b1d55314afe782508d6233bdc0efec573309d
Kim Yoo <kimyoo04eco@naver.com>
2023-02-13 01:31:31 +0900

    Fix CORS error

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 const origin = process.env.ORIGIN;
 app.use(
@@ -17,7 +18,17 @@ app.use(
 app.use(express.json());
 app.use(morgan("dev"));
 
-dotenv.config();
+// CORS 에러 해결
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", origin);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Content-Language, Accept, Accept-Language,"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", ["GET", "PUT", "POST", "DELETE"]);
+  next();
+});
 
 app.get("/", (_, res) => res.send("running"));
 app.use("/api/auth", authRoutes);

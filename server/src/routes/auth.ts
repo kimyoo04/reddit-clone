96d91c4af8d0fad2257a81d032804a78bb69d6e6
Kim Yoo <kimyoo04eco@naver.com>
2023-02-17 16:54:33 +0900

    Implement server sign in (JWT)

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,9 +1,7 @@
 import { Request, Response, Router } from "express";
 import { isEmpty, validate } from "class-validator";
-
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import cookie from "cookie";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import User from "../entities/User";
 
 const mapError = (errors: Object[]) => {
@@ -62,45 +60,140 @@ const signin = async (req: Request, res: Response) => {
   try {
     let errors: any = {};
 
-    // 비워져있다면 에러를 프론트엔드로 보내기
+    // 에러메시지 프론트로 응답
     if (isEmpty(email)) errors.email = "이메일을 입력해주세요.";
     if (isEmpty(password)) errors.password = "비밀번호를 입력해주세요.";
     if (Object.keys(errors).length > 0) return res.status(400).json(errors);
 
-    // 디비에서 유저 찾기
-    const user = await User.findOneBy({ email });
-    if (!user) return res.status(404).json({ email: "없는 사용자입니다." });
+    // 디비 유저 조회
+    const userData = await User.findOneBy({ email });
+    if (!userData) return res.status(404).json({ email: "없는 사용자입니다." });
 
-    // 유저가 있다면 비밀번호 비교
-    const passwordMatches = await bcrypt.compare(password, user.password);
+    // 유저 비밀번호 비교
+    const passwordMatches = await bcrypt.compare(password, userData.password);
     if (!passwordMatches)
       return res.status(401).json({ password: "비밀번호를 확인해주세요." });
 
-    // 비밀번호가 맞다면 토큰 생성
-    const token = jwt.sign(
-      { username: user.username, email },
-      process.env.JWT_SECRET!
-    );
+    // 비밀번호 제외
+    const givenData = {
+      id: userData.id,
+      username: userData.username,
+      email: userData.email,
+    };
+
+    // accessToken, refreshToken 생성 및 발급
+    const accessToken = jwt.sign(givenData, process.env.ACCESS_SECRET!, {
+      expiresIn: "1h", // 1시간
+      issuer: "Yoo Kim",
+    });
+    const refreshToken = jwt.sign(givenData, process.env.REFRESH_SECRET!, {
+      expiresIn: "7d", // 일주일
+      issuer: "Yoo Kim",
+    });
 
     // 쿠키 저장
-    res.set(
-      "Set-Cookie",
-      cookie.serialize("token", token, {
-        httpOnly: true,
-        maxAge: 60 * 60 * 24 * 7,
-        path: "/",
-      })
-    );
+    res.cookie("accessToken", accessToken, {
+      path: "/",
+      secure: false,
+      httpOnly: true,
+    });
+    res.cookie("refreshToken", refreshToken, {
+      path: "/",
+      secure: false,
+      httpOnly: true,
+    });
 
-    return res.status(200).json({ token });
+    return res.status(200).json({ message: "Sign in success." });
   } catch (error) {
     console.error(error);
     return res.status(500).json(error);
   }
 };
 
+const logout = async (req: Request, res: Response) => {
+  try {
+    res.cookie("accessToken", "", {
+      path: "/",
+      secure: false,
+      httpOnly: true,
+    });
+    res.cookie("refreshToken", "", {
+      path: "/",
+      secure: false,
+      httpOnly: true,
+    });
+
+    res.status(200).json({ message: "Log out success." });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
+const accessToken = async (req: Request, res: Response) => {
+  try {
+    const token = req.cookies.accessToken;
+    const data = jwt.verify(token, process.env.ACCESS_SECRET!) as JwtPayload;
+    const userData = await User.findOneBy({ email: data.email });
+    if (!userData) return res.status(404).json({ email: "없는 사용자입니다." });
+
+    res.status(200).json({ message: "Access token is valid." });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
+const refreshToken = async (req: Request, res: Response) => {
+  try {
+    const token = req.cookies.accessToken;
+    const data = jwt.verify(token, process.env.ACCESS_SECRET!) as JwtPayload;
+
+    const userData = await User.findOneBy({ email: data.email });
+    if (!userData) return res.status(404).json({ email: "없는 사용자입니다." });
+
+    // 비밀번호 제외
+    const givenData = {
+      id: userData.id,
+      username: userData.username,
+      email: userData.email,
+    };
+
+    // accessToken 생성 및 재발급
+    const accessToken = jwt.sign(givenData, process.env.ACCESS_SECRET!, {
+      expiresIn: "1h", // 1시간
+      issuer: "Yoo Kim",
+    });
+    res.cookie("accessToken", accessToken, {
+      path: "/",
+      secure: false,
+      httpOnly: true,
+    });
+
+    res.status(200).json({ message: "Access token is recreated" });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
+const signinSuccess = async (req: Request, res: Response) => {
+  const token = req.cookies.accessToken;
+  const data = jwt.verify(token, process.env.ACCESS_SECRET!) as JwtPayload;
+
+  const userData = await User.findOneBy({ email: data.email });
+  if (!userData) return res.status(404).json({ email: "없는 사용자입니다." });
+  const givenData = {
+    id: userData.id,
+    username: userData.username,
+    email: userData.email,
+  };
+  res.status(200).json(givenData);
+};
+
 const router = Router();
 router.post("/signup", signup);
 router.post("/signin", signin);
+router.post("/logout", logout);
+router.get("/accesstoken", accessToken);
+router.get("/refreshtoken", refreshToken);
+router.get("/signin/success", signinSuccess);
 
 export default router;
--- server/src/server.ts
+++ server/src/server.ts
@@ -4,22 +4,27 @@ import { AppDataSource } from "./data-source";
 import authRoutes from "./routes/auth";
 import cors from "cors";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config();
 
 const origin = process.env.ORIGIN;
+const methods = ["GET", "POST"];
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cookieParser());
 app.use(
   cors({
     origin,
+    methods,
     credentials: true, // 쿠키 전송
   })
 );
-app.use(express.json());
-app.use(morgan("dev"));
 
 // CORS 에러 해결
-app.use(function (req, res, next) {
+app.use(function (_, res, next) {
   res.header("Access-Control-Allow-Origin", origin);
   res.header(
     "Access-Control-Allow-Headers",
